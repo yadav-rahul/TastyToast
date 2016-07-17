5b1c440fb896e082f2a8fd3f2fdd50c19282dfb9
Rahul Yadav <rahuly247@gmail.com>
2016-07-17 10:37:38 +0530

    Modified .gitignore
    Signed-off-by: Rahul Yadav <rahuly247@gmail.com>

--- app/src/main/java/com/sdsmdg/tastytoast/TastyToast.java
+++ app/src/main/java/com/sdsmdg/tastytoast/TastyToast.java
@@ -12,28 +12,50 @@ import android.widget.Toast;
 
 public class TastyToast extends AppCompatActivity {
 
-    public static int LENGTH_SHORT = 0;
-    public static int LENGTH_LONG = 1;
+    public static final int LENGTH_SHORT = 0;
+    public static final int LENGTH_LONG = 1;
+
+    public static final int SUCCESS = 1;
+    public static final int DANGER = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasty_toast);
     }
-    public void showToast(View view) {
-        makeText(getApplicationContext(), "This is an example of Custom Toast.", TastyToast.LENGTH_LONG);
+
+    public void showSucessToast(View view) {
+        makeText(getApplicationContext(), "This is an example of Custom Toast.", TastyToast.LENGTH_LONG,
+                TastyToast.SUCCESS);
+    }
+
+    public void showDangerToast(View view) {
+        makeText(getApplicationContext(), "This is an example of Custom Toast.", TastyToast.LENGTH_LONG,
+                TastyToast.DANGER);
     }
 
 
-    public void makeText(Context context, String msg, int lenght) {
+    public void makeText(Context context, String msg, int lenght, int type) {
         LayoutInflater inflater = getLayoutInflater();
         View layout = inflater.inflate(R.layout.my_toast_layout,
                 (ViewGroup) findViewById(R.id.root_layout));
 
         TextView text = (TextView) layout.findViewById(R.id.toastMessage);
         text.setText(msg);
-        text.setBackgroundResource(R.drawable.sucess_toast);
+
+        text.setBackgroundResource(R.drawable.success_toast);
+        switch (type) {
+            case 1: {
+                text.setBackgroundResource(R.drawable.success_toast);
+                break;
+            }
+            case 2: {
+                text.setBackgroundResource(R.drawable.danger_toast);
+
+            }
+        }
         text.setTextColor(Color.parseColor("#FFFFFF"));
-        Toast toast = new Toast(getApplicationContext());
+        Toast toast = new Toast(context);
         toast.setDuration(lenght);
         toast.setView(layout);
         toast.show();
