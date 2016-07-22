97473f35ac693c12b29895085309a74b8b5feff2
Rahul Yadav <rahuly247@gmail.com>
2016-07-22 23:46:45 +0530

    Changed Image with Custom View

---
+++ app/src/main/java/com/sdsmdg/tastytoast/SuccessToastView.java
@@ -0,0 +1,134 @@
+package com.sdsmdg.tastytoast;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+/**
+ * Created by rahul on 22/7/16.
+ */
+public class SuccessToastView extends View {
+
+
+    RectF rectF = new RectF();
+    ValueAnimator valueAnimator;
+    float mAnimatedValue = 0f;
+    private Paint mPaint;
+    private float mWidth = 0f;
+    private float mEyeWidth = 0f;
+    private float mPadding = 0f;
+    private float endAngle = 0f;
+    private boolean isSmileLeft = false;
+    private boolean isSmileRight = false;
+
+    public SuccessToastView(Context context) {
+        super(context);
+    }
+
+
+    public SuccessToastView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SuccessToastView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initPaint();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        mWidth = getMeasuredWidth();
+        mPadding = dip2px(10);
+        mEyeWidth = dip2px(3);
+    }
+
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(Color.parseColor("#5cb85c"));
+        mPaint.setStrokeWidth(dip2px(2));
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        initPaint();
+        rectF = new RectF(mPadding, mPadding, mWidth - mPadding, mWidth - mPadding);
+        mPaint.setStyle(Paint.Style.STROKE);
+
+        canvas.drawArc(rectF, 180, endAngle, false, mPaint);
+
+        mPaint.setStyle(Paint.Style.FILL);
+        if (isSmileLeft) {
+            canvas.drawCircle(mPadding + mEyeWidth + mEyeWidth / 2, mWidth / 3, mEyeWidth, mPaint);
+
+        }
+        if (isSmileRight) {
+            canvas.drawCircle(mWidth - mPadding - mEyeWidth - mEyeWidth / 2, mWidth / 3, mEyeWidth, mPaint);
+        }
+    }
+
+    public int dip2px(float dpValue) {
+        final float scale = getContext().getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public void startAnim() {
+        stopAnim();
+        startViewAnim(0f, 1f, 2000);
+    }
+
+    public void stopAnim() {
+        if (valueAnimator != null) {
+            clearAnimation();
+            isSmileLeft = false;
+            isSmileRight = false;
+            mAnimatedValue = 0f;
+            valueAnimator.end();
+        }
+    }
+
+    private ValueAnimator startViewAnim(float startF, final float endF, long time) {
+        valueAnimator = ValueAnimator.ofFloat(startF, endF);
+        valueAnimator.setDuration(time);
+        valueAnimator.setInterpolator(new LinearInterpolator());
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+
+                mAnimatedValue = (float) valueAnimator.getAnimatedValue();
+                if (mAnimatedValue < 0.5) {
+                    isSmileLeft = false;
+                    isSmileRight = false;
+                    endAngle = -360 * (mAnimatedValue);
+                } else if (mAnimatedValue > 0.55 && mAnimatedValue < 0.7) {
+                    endAngle = -180;
+                    isSmileLeft = true;
+                    isSmileRight = false;
+                } else {
+                    endAngle = -180;
+                    isSmileLeft = true;
+                    isSmileRight = true;
+                }
+
+                invalidate();
+            }
+        });
+
+        if (!valueAnimator.isRunning()) {
+            valueAnimator.start();
+
+        }
+        return valueAnimator;
+    }
+}
--- app/src/main/java/com/sdsmdg/tastytoast/TastyToast.java
+++ app/src/main/java/com/sdsmdg/tastytoast/TastyToast.java
@@ -2,14 +2,11 @@ package com.sdsmdg.tastytoast;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,38 +14,30 @@ public class TastyToast extends AppCompatActivity {
 
     public static final int LENGTH_SHORT = 0;
     public static final int LENGTH_LONG = 1;
-
     public static final int SUCCESS = 1;
     public static final int DANGER = 2;
+    SuccessToastView successToastView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasty_toast);
+
     }
 
     public void showSuccessToast(View view) {
+        //TODO Show view in a smooth way
         makeText(getApplicationContext(), "Download Successful !", TastyToast.LENGTH_LONG,
                 TastyToast.SUCCESS);
     }
 
     public void showDangerToast(View view) {
+        //TODO Make separate view for it.
         makeText(getApplicationContext(), "Are you sure ?", TastyToast.LENGTH_LONG,
                 TastyToast.DANGER);
     }
 
-    public void showSuccessLongToast(View view) {
-        makeText(getApplicationContext(), "Download Successful ! Download Successful ! " +
-                "Download Successful ! Download Successful ! Download Successful !", TastyToast.LENGTH_LONG,
-                TastyToast.SUCCESS);
-    }
-
-    public void showDangerLongToast(View view) {
-        makeText(getApplicationContext(), "Are you sure ? Are you sure ? Are you sure ?" +
-                "Are you sure ? Are you sure ?", TastyToast.LENGTH_LONG,
-                TastyToast.DANGER);
-    }
-
+    //Custom toast method
 
     public void makeText(Context context, String msg, int length, int type) {
         LayoutInflater inflater = getLayoutInflater();
@@ -56,24 +45,27 @@ public class TastyToast extends AppCompatActivity {
                 (ViewGroup) findViewById(R.id.root_layout));
 
         TextView text = (TextView) layout.findViewById(R.id.toastMessage);
-        ImageView imageView = (ImageView) layout.findViewById(R.id.imageView);
         text.setText(msg);
-       // LinearLayout baseLayout = (LinearLayout) layout.findViewById(R.id.base_layout);
-
 
         switch (type) {
             case 1: {
-                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
-                imageView.setImageResource(R.drawable.success_image);
+//                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+//                imageView.setBackgroundResource(R.drawable.success_frame_animation);
+//
+//                AnimationDrawable animationDrawable = (AnimationDrawable) imageView.getBackground();
+//                animationDrawable.start();
+//                imageView.setAnimation(animation);
+                successToastView = (SuccessToastView) layout.findViewById(R.id.successView);
+                successToastView.startAnim();
                 text.setBackgroundResource(R.drawable.success_toast);
-                imageView.setAnimation(animation);
                 break;
             }
             case 2: {
-                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
-                imageView.setImageResource(R.drawable.warning_image);
-                text.setBackgroundResource(R.drawable.warning_toast);
-                imageView.setAnimation(animation);
+//                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
+//                imageView.setImageResource(R.drawable.warning_image);
+//                text.setBackgroundResource(R.drawable.warning_toast);
+//                imageView.setAnimation(animation);
+                break;
             }
         }
         text.setTextColor(Color.parseColor("#FFFFFF"));
